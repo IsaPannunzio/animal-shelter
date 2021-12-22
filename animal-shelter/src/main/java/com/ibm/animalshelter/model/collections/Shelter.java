7dc863a5a95a663bf6899b3a4527a3749872c3fb
Isabella Montejano Pannunzio Scudeler <isabellapannunzio@ibm.com>
2021-12-21 23:52:54 -0300

    Classe Shelter

---
+++ animal-shelter/src/main/java/com/ibm/animalshelter/model/collections/Shelter.java
@@ -0,0 +1,64 @@
+package com.ibm.animalshelter.model.collections;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.NotBlank;
+
+public class Shelter {
+
+    @Id
+    private String codigo;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String endereco;
+
+    @JsonFormat(pattern = " ")
+    @NotBlank
+    private String telefone;
+
+    public Shelter(String codigo, String nome, String endereco, String telefone) {
+
+        this.codigo = codigo;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
+
+
